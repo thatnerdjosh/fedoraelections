74f8ff121675cd4f94174563becefd8a29ffccab
root <nerdsville@nerdsville.net>
2014-08-25 07:40:07 -0700

    Adding default_config.py

---
+++ fedora_elections/default_config.py
@@ -0,0 +1,14 @@
+FEDORA_ELECTIONS_ADMIN_GROUP = 'elections'
+
+# url to the database server:
+DB_URL = 'sqlite:////var/tmp/elections_dev.sqlite'
+
+
+# You will want to change this for your install
+SECRET_KEY = 'change me'
+
+FAS_BASE_URL = 'https://admin.stg.fedoraproject.org/accounts/'
+FAS_USERNAME = ''
+FAS_PASSWORD = ''
+FAS_CHECK_CERT = False
+
